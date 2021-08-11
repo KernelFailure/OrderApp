428c202d3ea5ee359657267e21a51c434b4cffbe
kernelfailure <leonp1mcg@gmail.com>
2021-08-11 14:22:53 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from app import app
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
